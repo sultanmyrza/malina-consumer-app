1aaa8969253ca9324975e65742fd9d0ef2ce70d9
sultanmyrza <sultanmyrza@gmail.com>
2024-05-03 20:32:05 +0600

    feat: Implement UI for barcode banner widget
    This commit adds the UI implementation for the barcode banner widget in the HomeTabPage. The widget now displays the necessary design elements according to the Figma design specifications.

--- lib/features/home/home_tab/home/widgets/barcode_banner.dart
+++ lib/features/home/home_tab/home/widgets/barcode_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:malina_consumer_app/theme/colors.dart';
 
 class BarcodeBanner extends StatelessWidget {
@@ -6,23 +7,43 @@ class BarcodeBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: @sultanmyrza implement BarcodeBanner widget
-    return Column(
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          width: double.infinity,
-          height: 90,
-          padding: const EdgeInsets.all(16),
-          clipBehavior: Clip.antiAlias,
-          decoration: ShapeDecoration(
-            color: AppColors.malina,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
+    return GestureDetector(
+      onTap: () => {
+        // TOOD: @sultanmyrza navigate to barcode scanner page
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        height: 90,
+        padding: const EdgeInsets.all(16),
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          color: AppColors.malina,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
         ),
-      ],
+        child: Row(
+          children: [
+            SvgPicture.asset("assets/icons/barcode_icon.svg"),
+            const SizedBox(width: 16),
+            const Flexible(
+              child: Text(
+                // TODO: @sultanmyrza localize
+                'Сканируй QR-код и заказывай прямо в заведении',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'SF Pro Display',
+                  fontWeight: FontWeight.w500,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
