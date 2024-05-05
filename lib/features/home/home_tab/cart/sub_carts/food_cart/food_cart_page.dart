e2136ab9be39bbbcc8d7352f6241c713fc4dd67f
sultanmyrza <sultanmyrza@gmail.com>
2024-05-05 16:52:01 +0600

    WIP: add placeholder pages for foods, goods subcart

---
+++ lib/features/home/home_tab/cart/sub_carts/food_cart/food_cart_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodsCartPage extends StatelessWidget {
+  const FoodsCartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Center(child: Text("FoodsCartPage")));
+  }
+}
